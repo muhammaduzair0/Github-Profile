1a180a57e7546d8b4cc950c49bda02ab87036c8a
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-02-22 18:16:34 +0500

    Function Added

---
+++ script.js
@@ -0,0 +1,10 @@
+// const url = `https://api.github.com/users/${searchValue}`
+
+const searchInput = document.querySelector('#searchInput')
+
+const getSearchValue = () => {
+    userValue = searchInput.value
+
+    console.log(userValue)
+}   
+
