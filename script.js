7d42cbc9821dbacbdb9f5e4cd75313d79fae9a66
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-02-25 13:53:43 +0500

    API Url Fetched

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ const getSearchValue = () => {
     searchValue = searchInput.value
 
     console.log(searchValue)
+
+    
+const url = `https://api.github.com/users/${searchValue}`
+
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data)
+    }
+)
 }   
 
 searchInput.addEventListener('input', getSearchValue)
 
-const url = `https://api.github.com/users/${searchValue}`
 
 
