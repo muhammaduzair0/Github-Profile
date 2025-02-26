ad06ba6a0eebcb18b8b77620e0f4d64ebdcc8cea
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-02-26 14:59:38 +0500

    functionality added

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let searchValue = document.getElementById("searchInput");
 let searchBtn = document.getElementById("searchBtn");
-let profileAvatar = document.getElementById("avatar")
-let bio = document.getElementById("bio")
-let repos = document.getElementById("repos")
-let followers = document.getElementById("followers")
-let following = document.getElementById("following")
-let profileLink = document.getElementById("profileLink")
+let profileCard = document.getElementById("profileCard");
+let profileAvatar = document.getElementById("avatar");
+let bio = document.getElementById("bio");
+let repos = document.getElementById("repos");
+let followers = document.getElementById("followers");
+let following = document.getElementById("following");
+let profileLink = document.getElementById("profileLink");
 
 // FUNCTION FOR API FETCH AND GETTING SEARCH VALUE FROM INPUT
 const getSearchValue = () => {
@@ -19,6 +20,20 @@ const getSearchValue = () => {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
+      // DISPLAYING PROFILE CARD
+      profileCard.style.display = "block";
+      // SETTING PROFILE AVATAR
+      profileAvatar.src = data.avatar_url;
+      // SETTING BIO
+      bio.textContent = data.bio;
+      // SETTING REPOS
+      repos.textContent = data.public_repos;
+      // SETTING FOLLOWERS
+      followers.textContent = data.followers;
+      // SETTING FOLLOWING
+      following.textContent = data.following;
+      // SETTING PROFILE LINK
+      profileLink.href = data.html_url;
     });
 };
 
